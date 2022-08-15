b64299431ba344c3c172e7a91cb17680c5f74a3b
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-08-15 14:17:48 -0500

    starting boolean and conditional section

---
+++ 8_Boolean_and_conditional_logic/decisions.py
@@ -0,0 +1,8 @@
+name = input('Write your name here: ')
+
+if name == 'Cornelio':
+    print('You are Cornelio')
+elif name == 'Oksana':
+    print('You are Oksana')
+else:
+    print('You are not Cornelio')
---
+++ 8_Boolean_and_conditional_logic/inputs.py
@@ -0,0 +1,2 @@
+name = input('Enter your name here: ')
+print('Your name is ' + name)
