a3bb80e08a70896109c3b75765e7c3e33650a6b5
Posadas Cornelio <PosadasCornelio@JohnDeere.com>
2022-08-22 14:46:56 -0500

    working on comparison operators

---
+++ 8_Boolean_and_conditional_logic/Comparison operators.py
@@ -0,0 +1,18 @@
+## Comparison operators NOTES
+
+## a == b
+## a != b
+## a < b
+## a > b
+## a <= b
+## a >= b
+
+num = input('Give me a number: ')
+num1 = input('Give me another number: ')
+
+if num == num1:
+    print(f'{num} is equal to {num1}')
+elif num < num1:
+    print(f'{num} is minor than {num1}')
+elif num > num1:
+    print(f'{num} is greater than {num1}')
