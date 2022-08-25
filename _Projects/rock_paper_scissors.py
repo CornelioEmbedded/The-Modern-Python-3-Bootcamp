256df18e5256e61a69833f48f050a635fcb66400
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-08-25 08:22:18 -0500

    adding concatenate if

--- _Projects/rock_paper_scissors.py
+++ _Projects/rock_paper_scissors.py
@@ -7,18 +7,24 @@ print('... Rock...')
 print('...Paper...')
 print('...Scissors...')
 
-choice1 = input(' Enter Player 1´s choice: ')
-choice2 = input(' Enter Player 2´s choice: ')
+choice1 = input(' Enter Player 1 choice: ')
+choice2 = input(' Enter Player 2 choice: ')
 
-if choice1 == 'paper' and choice2 == 'rock':
-    print('Player 1 wins!')
-elif choice1 == 'rock' and choice2 == 'scissors':
-    print('Player 1 wins!')
-elif choice1 == 'scissors' and choice2 == 'paper':
-    print('Player 1 wins!')
-if choice2 == 'paper' and choice1 == 'rock':
-    print('Player 2 wins!')
-elif choice2 == 'rock' and choice1 == 'scissors':
-    print('Player 2 wins!')
-elif choice2 == 'scissors' and choice1 == 'paper':
-    print('Player 2 wins!')
+if choice1 != choice2:
+    if choice1 ==  'rock':
+        if choice2 == 'paper':
+            print('player 2 wins!')
+        elif choice2 == 'scissors':
+            print('player 1 wins!')
+    elif choice1 == 'paper':
+        if choice2 == 'rock':
+            print('player 1 wins!')
+        elif choice2 == 'scissors':
+            print('player 2 wins!')
+    elif choice1 == 'scissors':
+        if choice2 == 'rock':
+            print('player 2 wins!')
+        elif choice2 == 'paper':
+            print('player 1 wins!')
+else:
+    print('Its a tie')
