4d1f95c5266cee154e8ca2c2c2a7d797163a4160
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-08-29 15:17:42 -0500

    work on lists

---
+++ 12_Lists/lists.py
@@ -0,0 +1,16 @@
+
+## Just playing with lists
+
+# numbers = [0xA5, 0xAE, 0xAD]
+
+# for number in numbers:
+#     print(number)
+
+sounds = ["super", "cali", "fragil", "istic", "expi", "ali", "docious"]
+
+# Define your code below:
+result = ''
+for s in sounds:
+    result += s
+    
+print(result.upper())
