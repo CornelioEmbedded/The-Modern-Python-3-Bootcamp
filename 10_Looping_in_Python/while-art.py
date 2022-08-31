7bc130e4392c3ab754b991b5b819a81ccfed9ca6
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-08-31 12:27:32 -0500

    Merge branch 'main'

---
+++ 10_Looping_in_Python/while-art.py
@@ -0,0 +1,11 @@
+## print('\U0001f600')  ## emoji code
+
+max = input('how many  rows do you want: ')
+
+for n in range(0,int(max)):
+   print('\U0001f600'*n)
+
+num = 1
+while num < int(max):
+    print('\U0001f600'*num)
+    num += 1
---
+++ 12_Lists/list-methods.py
@@ -0,0 +1,18 @@
+## add - add one elemen
+## append - add another item at the end
+## extend - add more than 1 item at the end
+## insert - add in any position 1 item
+## clear - remove all items from the list
+## pop - remove item from one position
+## remove - remove the selected item, if it is more values, it remove the first one
+## index - gives the place where the item is
+## count - return the number of times the item appears
+## reverse - reverse the order of the list
+## sort - sorts items of the list
+## join - join strings
+
+first = ['Cornelio', 'is', 'learning']
+
+' '.join(first)
+
+print(' '.join(first))
