cb0aef8e77665fe97a15e82bf30948ad96b1e4ae
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-09-01 10:45:01 -0500

    finish working on list comprehension

--- 13_List_Comprehension/list-comprenhesion.py
+++ 13_List_Comprehension/list-comprenhesion.py
@@ -12,8 +12,20 @@ numbers = [1,2,3,4,5]
 
 
 
-answer = [val for val in [1,2,3,4] if val in [3,4,5,6]]
-#the slice [::-1] is a quick way to reverse a string
-answer2 = [val[::-1].lower() for val in ["Elie", "Tim", "Matt"]]
+# answer = [val for val in [1,2,3,4] if val in [3,4,5,6]]
+# #the slice [::-1] is a quick way to reverse a string
+# answer2 = [val[::-1].lower() for val in ["Elie", "Tim", "Matt"]]
 
-print(answer)
+# answer = [val for val in range(1,101) if val % 12 == 0] 
+
+# print(answer)
+
+# n = range(0,10)
+
+# print(list(n))
+
+nested = [[1,2,3], [4,5,6], [7,8,9]]
+
+for l in nested:
+    for val in l:
+        print(val)
