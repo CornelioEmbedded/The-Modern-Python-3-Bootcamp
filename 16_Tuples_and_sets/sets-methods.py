cf77b73fa89e1e92001cce6b4bbed1fd2c8795ea
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-09-02 14:45:57 -0500

    finish with tuples and sets section

---
+++ 16_Tuples_and_sets/sets-methods.py
@@ -0,0 +1,5 @@
+## add - adds an element to a set
+## remove - remove a value from a set
+## discard - remove a value from a set too
+## copy- makes a copy of the set
+## clear - removes all the content of a set
---
+++ 16_Tuples_and_sets/sets.py
@@ -0,0 +1,6 @@
+## sets ---> unordered collection of unique values
+
+s = set({1,2,3})
+
+for n in s:
+    print(n)
