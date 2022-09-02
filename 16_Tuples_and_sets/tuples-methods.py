5acabba75e8e7e7bda2fb4b8f9c76434a76c2bbd
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-09-02 14:17:53 -0500

    start with sets

---
+++ 16_Tuples_and_sets/tuples-methods.py
@@ -0,0 +1,7 @@
+
+## count - number of times the value is in tuple
+## index - return which value is inside in the tuple
+
+names = ('Hola', 'mi', 'nombre', 'es', 'cornelio')
+
+print(names.index('Hola'))
---
+++ 16_Tuples_and_sets/tuples.py
@@ -0,0 +1,5 @@
+## Tuples ---> Lists that doesnt change
+
+alph = ('a', 'b', 'c', 'd')
+
+alph[0] = 'A'
