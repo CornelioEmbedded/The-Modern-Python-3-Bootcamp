75d413ea6f1d0fa0f2e967cf794864e97b856e31
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-09-02 11:23:34 -0500

    finish with dictionaries

--- _Projects/Guessing-Game/guessing-game.py
+++ _Projects/Guessing-Game/guessing-game.py
@@ -6,18 +6,30 @@ import random
 # Generate one random number
 num = random.randint(1,10)
 
+tries = 1
+num_tries = 5
+
 # Getting a number from the user
-selection = int(input('Type a number from 1 to 10: '))
+selection = int(input('\nType a number from 1 to 10: '))
 
 # Making loop for selection
+
 while num != selection:
 
     print('Wrong number!')
     print(f'Random number: {num}')
+
+    tries += 1
 
     num = random.randint(1,10)
 
     selection = selection = int(input('Type a number from 1 to 10: '))
 
 print(f'Random number: {num}')
-print('You guess it!')
+print('\nYou guess it!')
+print(f'\nNumber of tries: {tries}')
+
+if tries > num_tries :
+    print('You are an idiot :D')
+else:
+    print('You are smart :)')
