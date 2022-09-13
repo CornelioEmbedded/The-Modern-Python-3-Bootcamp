799c6f45e3bebc4415d37032512f3cba5944d06a
Cornelio Posadas <PosadasCornelio@JohnDeere.com>
2022-09-13 13:54:43 -0500

    work on lambda

---
+++ 20_Lambdas_and_Built-In_Functions/lambda.py
@@ -0,0 +1,22 @@
+## lambda
+
+# hola = lambda num: num **3
+
+# print(hola(3))
+
+## map
+
+# num = [2,4,6,8,10]
+
+# doubles = map(lambda x: x * 2 , num)
+
+# for num in doubles:
+#     print(num),
+
+## all
+
+# people = ['Charlie', 'Casey', 'Cody', 'Cornelio']
+# print(all([name[0] == 'C' for name in people]))
+
+
+
